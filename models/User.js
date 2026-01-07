ef18e5665ad9c73131689889505cd6bde5efecce
Yosef770a <moked7700@gmail.com>
2026-01-07 16:01:10 +0200

    fix: Removing the requirement to accept an ID as an argument

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export class User { 
-    constructor(id, username, password) {
+    constructor(username, password) {
         this.id = uuidv4()
         this.username = username
         this.password = password
